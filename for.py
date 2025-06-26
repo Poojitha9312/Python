3542830517251e01339c6cd01778c64d53d47e51
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-26 15:18:02 +0530

    done

--- for.py
+++ for.py
@@ -1,5 +1,5 @@
 # for-loop:
-# for loop executes the set of statements with repeat to every elemnet of the string/ collection objects
+# for loop executes the set of statements with repeat to every elemnet of the string/ collection object
 x=[10,20,30,40]
 print(x)
 
