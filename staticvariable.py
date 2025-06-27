e2113649bcd0c711555f0e7275b33c6a9e4b59f6
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-27 22:22:37 +0530

    done

--- staticvariable.py
+++ staticvariable.py
@@ -5,3 +5,13 @@
 #--------when should we go for static variables---------
 #  if the value of the variable is fixed for all obects
 # then we can go for static variable.....
+
+#----CREATION OF STATIC VARIABLE IN SIX METHODS-----
+# 1.Inside class directly
+# 2.outside of the class
+# 3.inside the constructor
+# 4.inside instance method
+# 5.Inside class method
+# 6.inside static method....
+
+
