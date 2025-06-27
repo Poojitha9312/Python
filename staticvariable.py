62fb3faaef6105c400929db2e0641633016ce754
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-27 22:14:49 +0530

    done

---
+++ staticvariable.py
@@ -0,0 +1,7 @@
+#------------STATIC VARIABLE---------------
+# It is also called as class level variable
+# static variable belongs to class not object
+
+#--------when should we go for static variables---------
+#  if the value of the variable is fixed for all obects
+# then we can go for static variable.....
