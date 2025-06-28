b0455aa2e215ac9b15a0eb17dfa9631dd84d9c4d
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-28 09:38:03 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,7 @@
-class Person:
-    def __init__(self,name,age,rollno):
-        self.name=name 
-        self.age=age 
-        self.rollno=rollno 
-p1=Person("Ram",30,32)
-print(p1.__dict__)
-
-del p1.rollno
-print(p1.__dict__)
-   
+l=[10,20,30,"poojitha",8.2,"snehitha"]
+print(l)
+print(l[::])
+print(l[0:4])
+print(l[-1])
+print(l[-3:])
+print(l[-5:])
