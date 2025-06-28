95e54f689a58459929fefd3825431db240a02381
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-28 12:07:47 +0530

    done

---
+++ instancemethod.py
@@ -0,0 +1,6 @@
+#---------------INSTANCE METHOD--------------------------
+# Inside the instance method we use instance variables.
+# The first argument of the instance method is self.
+# We can call one instance method inside another instance method.
+# Inside the class we can call instance method using self variable and outside the class we can call throught object reference.
+# By using instance method we can perform various operations like modify, delete, etc.
