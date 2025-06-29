a41a9253d87d782d4551f3d6d74825affae0fbab
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-29 17:42:42 +0530

    done

--- classmethod.py
+++ classmethod.py
@@ -36,6 +36,28 @@ student.cm()
 # s1.sample()
 
 
+#Example: accessing one classmethod variables to another classmethod
+class student:
+    school_name="Pragati"
+    @classmethod
+    def cm(cls):
+        cls.school_location="kkd"
+        cls.school_pincode=533005
+        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
+    def sample(cls):
+        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
+
+student.cm()# we can call by class name
+s1=student()
+s1.cm()
+s1.sample()
+
+#Example 4: By using class method we can perform several operation to static variables.
+
+
+
+
+
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,14 @@
 class student:
-    school_name="Aditya"
+    school_name="Pragati"
     @classmethod
     def cm(cls):
-        cls.school_location="rjy"
-        print(f"My schoolname is {cls.school_name} and my school location is {cls.school_location}")
-        student.sample(cls)#here calling class method in another class method
+        cls.school_location="kkd"
+        cls.school_pincode=533005
+        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
     def sample(cls):
-        cls.school_principalname="haritha"
-        print(f" my principal name is {cls.school_principalname}")
+        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
 
-student.cm()
+student.cm()# we can call by class name
+s1=student()
+s1.cm()
+s1.sample()
