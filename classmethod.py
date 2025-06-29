2e522750cf776004b45fbeb8ad16b048fac29582
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-29 17:47:01 +0530

    done

--- classmethod.py
+++ classmethod.py
@@ -53,9 +53,28 @@ s1.cm()
 s1.sample()
 
 #Example 4: By using class method we can perform several operation to static variables.
+class Test:
+    a=10
+    b=20
+    
+    @classmethod
+    def access(cls):
+        print(f"The value of a is {cls.a} and the value of b is {cls.b}")
+    
+    @classmethod
+    def update(cls,x,y):
+        cls.a=x
+        cls.b=y
 
+    @classmethod
+    def delete(cls):
+        del cls.a
 
-
+Test.access()
+Test.update(30,40)
+Test.access()
+Test.delete()
+print(Test.__dict__)
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,22 @@
-class student:
-    school_name="Pragati"
+class Test:
+    a=10
+    b=20
+    
     @classmethod
-    def cm(cls):
-        cls.school_location="kkd"
-        cls.school_pincode=533005
-        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
-    def sample(cls):
-        print(f" My schhol name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
+    def access(cls):
+        print(f"The value of a is {cls.a} and the value of b is {cls.b}")
+    
+    @classmethod
+    def update(cls,x,y):
+        cls.a=x
+        cls.b=y
 
-student.cm()# we can call by class name
-s1=student()
-s1.cm()
-s1.sample()
+    @classmethod
+    def delete(cls):
+        del cls.a
+
+Test.access()
+Test.update(30,40)
+Test.access()
+Test.delete()
+print(Test.__dict__)
