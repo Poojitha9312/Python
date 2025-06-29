5aebae9eb8b553db46bb0d49fe6ed93047d12bcd
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-29 17:23:36 +0530

    done

--- classmethod.py
+++ classmethod.py
@@ -25,13 +25,16 @@ class student:
     @classmethod
     def cm(cls):
         cls.school_location="rjy"
-        print(f"My schoolname is {cls.school_name} and my location is {cls.school_location}")
+        print(f"My schoolname is {cls.school_name} and my school location is {cls.school_location}")
         student.sample(cls)#here calling class method in another class method
     def sample(cls):
         cls.school_principalname="haritha"
         print(f" my principal name is {cls.school_principalname}")
 
 student.cm()
+
+#output:
+My schoolname is Aditya and my 
 
 
 
