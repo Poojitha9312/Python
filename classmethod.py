b8fdb570cad2a02fc9e6e8ad07d2d1961aaa47b5
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-29 17:07:34 +0530

    done

--- classmethod.py
+++ classmethod.py
@@ -5,3 +5,15 @@
 # we can call one class method inside the another class method.
 # Inside the class we can call class method using class name and outside the class we can call throught class name or object reference.
 # By using class method we can perform various operations like modify, delete, etc.
+
+# Example:
+class student:
+    school_name="hamsavahini"
+    @classmethod
+    def cm(cls):
+        cls.school_location="kkd"# create class variable by using cls
+        student.school_pincode=533005
+        print(f"my school name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
+student.cm()
+s1=student()
+s1.cm()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,10 @@
-def hide_char():
-    name="rama"
-    for i in name:
-        if i=='a':
-            continue
-        print(i)
-hide_char()
+class student:
+    school_name="hamsavahini"
+    @classmethod
+    def cm(cls):
+        cls.school_location="kkd"# create class variable by using cls
+        student.school_pincode=533005
+        print(f"my school name is {cls.school_name} and my school location is {cls.school_location} and my school pincode is {cls.school_pincode}")
+student.cm()
+s1=student()
+s1.cm()
