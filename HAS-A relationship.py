96a6cef9c5ef4a2e84bf1b807b984c66624bc81d
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-30 19:32:52 +0530

    done

--- HAS-A relationship.py
+++ HAS-A relationship.py
@@ -20,4 +20,38 @@ class car:
         self.cname=cname
         self.ccolour=ccolour
         self.cprice=cprice
-    def 
+    def car_details(self,ename):
+        print(f"{ename}carname is {self.cname}")
+        print(f"{ename}car colour is {self.ccolour}")
+        print(f"{ename}car price is {self.cprice}")
+        print("*"*20)
+
+
+
+class laptop:
+    def __init__(self,lname,lcolour,lprice):
+        self.lname=lname
+        self.lcolour=lcolour
+        self.lprice=lprice
+
+    def laptop_details(self,ename):
+        print(f"{ename} laptopname is {self.lname}")
+        print(f"{ename}  laptop colour is {self.lcolour}")
+        print(f" {ename} laptop price is {self.lprice}")
+
+
+
+class employee:
+    def __init__(self,ename,eage,eid):
+        self.ename=ename
+        self.eage=eage
+        self.eid=eid
+        self.l =laptop("Lenovo", "Grey", "40K")
+    def employee_details(self):
+        print(f"my name is {self.ename}")
+        print(f"my age is {self.eage}")
+        print(f"my id is {self.eid}")
+        self.l.laptop_details(self.ename)  # Pass the emp_name to the Laptop's details
+s1=employee("ram",30,1026)
+s1.employee_details()
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,11 @@
-def hide_char():
-    name="Rama"
-    for i in name:
-        if i=="a":
-            continue
-        print(i)
-hide_char()
+class employee:
+    def __init__(self,ename,eage,eid):
+        self.ename=ename
+        self.eage=eage
+        self.eid=eid
+    def employee_details(self):
+        print(f"my name is {self.ename}")
+        print(f"my age is {self.eage}")
+        print(f"my id is {self.eid}")
+s1=employee("ram",30,1026)
+s1.employee_details()
