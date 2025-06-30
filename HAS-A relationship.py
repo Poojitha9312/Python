df3a818da298807fceb9fef737750df0176050d1
Poojitha9312 <pujivasu2011@gmail.com>
2025-06-30 16:26:29 +0530

    done

--- HAS-A relationship.py
+++ HAS-A relationship.py
@@ -2,5 +2,17 @@
 # The advantage of relationship is code-reusability....
 # HAS-A relationship(also known as composition)
 # IS-A relationship(Inheritance)
-# In HAS-A relationship we will build object from smaller objects...
+# Examples:
+
+# 1. Employee HAS-A car
+# 2. Employee IS-A Person
+# 3. Car HAS-A Engine
+# 4. Car IS-A Vehicle
+
+# #1. HAS-A Relationship
+# HAS-A Relationship is also known as Composition or Containership or Complex Objects.
+# In HAS-A Relationship we will build objects from smaller objects.
+# Ex: College is complex object and departments are smaller objects.
+
+# Here without College(Container Object) there are no departments(Contained objects).
 
