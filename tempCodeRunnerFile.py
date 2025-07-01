a784084b823bd46c8b18cd7166e9e50d6c1b4ca7
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-01 09:56:14 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,6 @@
 class student:
-    @classmethod
-    def class_method(cls):  #here for class method 1st variable is "cls" like "self"
-        student.school_name="Sri Chaitanya" #static variable
-        
-#Here in this case no need to create object we directly call class methods with class name reference.
-student.class_method()
-print(student.__dict__)
+    college_name="sri chaitanya"
+    @staticmethod
+    def sample():
+        print(student.college_name)
+student.sample()
