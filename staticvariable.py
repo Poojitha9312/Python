13ac77af73f920c5fce1fbef91c127b8f9cb7522
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-01 09:37:57 +0530

    done

--- staticvariable.py
+++ staticvariable.py
@@ -32,7 +32,7 @@ class student:
 s1=student()
 student.school_name="Sri Chaitanya" #static variable
 print(student.__dict__)
-
+#here we can create with object also
 s1.school_name="Sri Chaintanya"
 print(student.__dict__)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,4 @@
-class employee:
-    def __init__(self,ename,eage,eid):
-        self.ename=ename
-        self.eage=eage
-        self.eid=eid
-    def employee_details(self):
-        print(f"my name is {self.ename}")
-        print(f"my age is {self.eage}")
-        print(f"my id is {self.eid}")
-s1=employee("ram",30,1026)
-s1.employee_details()
+class student:
+    school_name="hamsavahini" #static variable
+
+print(student.school_name)  
