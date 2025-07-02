20629e78b3972f1db005f34dfa51953dc2f987b6
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-02 20:29:58 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,20 @@
-class student:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def sample(self):
-        print(f"My name is {self.name}")
-        print(f"My age is {self.age}")
-        print(30*'=')
-s1=[student("aparna",15),student("kavya",20)]
-for student in s1:
-    student.sample()
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def Details(self):
+        print(f'Name is {self.name}')
+        print(f'Age is {self.age}')
+
+class Staff:
+    @classmethod
+    def modify_student(cls, student_obj):  # class method
+        student_obj.age = 31
+        print("Student age modified by class method in Staff")
+
+s1 = Student("Ram", 30)
+s1.Details()
+
+Staff.modify_student(s1)  # class method can be called from class
+s1.Details()
