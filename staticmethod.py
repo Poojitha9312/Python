77709f07d20a5a0ee1d2c41119f0057ded5891c3
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-02 20:44:12 +0530

    done

--- staticmethod.py
+++ staticmethod.py
@@ -9,15 +9,25 @@
 #Example 1:
 class calculator:
     @staticmethod
-    def add(a=10,b=20):
+    def add():
+        a=10
+        b=20
+        result=a+b
         print(f"Addition result is {a+b}")
-        
-    def sub(a,b):
+    @staticmethod
+    def sub():
+        a=10
+        b=20
+        result=a-b
         print(f"Subtraction result is {a-b}")
         
 calculator.add() #calling static method using class name
 c1=calculator()
-c1.add()  #calling static method using object reference
+c1.add()
+calculator.sub()#calling static method using class name
+c1.sub()
+
+
 
 #Example 2: calling static method inside the class
 class calculator:
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,19 @@
 class calculator:
     @staticmethod
-    def add(a=10,b=20):
+    def add():
+        a=10
+        b=20
+        result=a+b
         print(f"Addition result is {a+b}")
-        
+    @staticmethod
     def sub(a,b):
+        a=10
+        b=20
+        result=a-b
         print(f"Subtraction result is {a-b}")
         
 calculator.add() #calling static method using class name
 c1=calculator()
 c1.add()
+calculator.sub()#calling static method using class name
+c1.sub()
