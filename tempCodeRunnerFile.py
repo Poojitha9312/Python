98114a2d72931edeea8f1621c7525cec3eeeeff2
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-02 09:59:32 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,23 @@
-class calculator:
-    def add(self):
-        a=20
-        b=30
-        result=a+b
-        print(result)
+class Test:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        
+    def access(self):
+        print(f"The value of a is {self.a} and the value of b is {self.b}")
+    
+    def update(self,x,y):
+        self.a=x
+        self.b=y
 
-    def sub(self):
-        print(a)
-        print(b)
-c1=calculator()
-c1.add()
+    def delete(self):
+        del self.a
+
+        
+t1=Test(10,20)
+print(t1.__dict__)
+t1.access()
+t1.update(30,40)
+t1.access()
+t1.delete()
+print(t1.__dict__)
