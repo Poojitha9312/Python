66b4dd36c36c663850a17b47c6af87ddd4346d5c
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-02 20:55:46 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,9 @@
-class calculator:
-    @staticmethod
-    def add():
-        a=10
-        b=20
-        result=a+b
-        print(f"Addition result is {a+b}")
-    # @staticmethod
-    def sub():
-        a=10
-        b=20
-        result=a-b
-        print(f"Subtraction result is {a-b}")
-        
-calculator.add() #calling static method using class name
-c1=calculator()
-c1.add()
-calculator.sub()#calling static method using class name
-c1.sub()
+class Outer:
+    def __init__(self):
+        print("Outer class")
+    class Inner:
+        def __init__(self):
+            print("Inner Class") 
+    i=Inner()   
+    
+o=Outer()
