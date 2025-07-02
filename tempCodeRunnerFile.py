b8218a8a04bd73dfc0908930c92ca3fda0c7c500
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-02 09:47:03 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,12 @@
-m="WELCOME TO HIE TECH SOLITIONS"
-l=m.split('O')
-print(l)
+class calculator:
+    def add(self):
+        a=20
+        b=30
+        result=a+b
+        print(result)
+
+    def sub(self):
+        print(a)
+        print(b)
+c1=calculator()
+c1.add()
