bbb8dbd4a38f245c164edf455a7bb5b2f94bf6bd
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-03 19:11:16 +0530

    done

--- multilevel inheritance.py
+++ multilevel inheritance.py
@@ -3,3 +3,28 @@
 
 #Example:
 #class A ------> class B(A) ------> class C(B)
+
+#Example:
+class A:
+    def method1(self):
+        print("This is parent A class Method1")
+        
+class B(A):
+    def method2(self):
+        print("This is child B class Method2")
+    
+class C(B):
+    def method3(self):
+        print("This is grandchild C class Method3")
+        
+a=A()   #A have only A class method
+a.method1()
+
+b=B()  #B have both A and B class methods
+b.method1()
+b.method2()
+
+c=C()  #C have A,B,C class methods
+c.method1()
+c.method2()
+c.method3()
