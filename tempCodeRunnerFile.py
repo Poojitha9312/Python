ba8765d851dbfb3af09cb677e98fd46b893bb109
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-03 16:11:56 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,16 @@
-class Outer:
-    def __init__(self):
-        print("Outer class")
-    class Inner:
-        def __init__(self):
-            print("Inner Class") 
-    i=Inner()   
-    
-o=Outer()
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        
+    def Details(self):
+        print(f'Name is {self.name}')
+        print(f'Age is {self.age}')
+
+class Staff:
+    @staticmethod
+    def modify_age(x): 
+        print(x) 
+
+s1=Student("Ram",30)
+Staff.modify_age(s1.age)
