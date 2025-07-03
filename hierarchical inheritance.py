304d5e2cf4b33a4ef3c24ae2800f12e805f1be0e
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-03 19:22:00 +0530

    done

--- hierarchical inheritance.py
+++ hierarchical inheritance.py
@@ -10,3 +10,21 @@ Multiple classes are derived from a single class.
        *               *
     class B(A)      class C(A)
 '''
+
+#Example:
+class A:
+    def method1(self):
+        print("Parent class method1")
+class B(A):
+    def method2(self):
+        print("Child class method2")
+class C(A):
+    def method3(self):
+        print("Child class method3")
+b=B()
+b.method1()
+b.method2()
+
+c=C()
+c.method1()
+c.method3 ()  
