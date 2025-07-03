d833bb7a9f641faab8682f1a3a3f19b29d4df5eb
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-03 16:58:21 +0530

    done

--- IS-A_Relationship.py
+++ IS-A_Relationship.py
@@ -46,3 +46,24 @@ b.method3()
 b.method1() #Here B object has no attribute 'method1'
 
 #Example:
+class A:
+    def m1(self):
+        x="ram"
+        print(x)
+    def m2(self):
+        y="ruchi"
+        print(y)
+    def m3(self):
+        z="kaveri"
+class B:
+    def sample(self):
+        a=20
+        b=10
+        print(a+b)
+b1=B()
+b1.sample()
+b1.m2()# here it doesn't work it will raise attribute error
+
+        
+        
+   
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,31 @@
-class Student:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-        
-    def Details(self):
-        print(f'Name is {self.name}')
-        print(f'Age is {self.age}')
+class A:
+    def method1(self):
+        print("A class Method1")
+    def method2(self):
+        print("A class method2")
+class B:
+    def method3(self):
+        print("B class method3")
 
-class Staff:
-    @staticmethod
-    def modify_age(x): 
-        print(x) 
+b=B()
+b.method3()
+b.method1() #Here B object has no attribute 'method1'
 
-s1=Student("Ram",30)
-Staff.modify_age(s1.age)
+#Example:
+class A:
+    def m1(self):
+        x="ram"
+        print(x)
+    def m2(self):
+        y="ruchi"
+        print(y)
+    def m3(self):
+        z="kaveri"
+class B:
+    def sample(self):
+        a=20
+        b=10
+        print(a+b)
+b1=B()
+b1.sample()
+b1.m2()
