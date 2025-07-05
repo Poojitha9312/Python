db2e5b96591ecbdccb8c0803e27a5209c8e057e3
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 10:17:03 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,15 @@
-l=[2,4,3,1,5,8,7]
-# i=0
-for i in l:
-    l.sort()
-print(l)
+class A:
+    def m1(self):
+        self.x=10
+        self.y=20
+        print(self.x)
+        print(self.y)
+    def m2(self):
+        self.z=30
+        print(self.z)
+class B(A):
+    pass
+
+b1=B()
+b1.m2()
+b1.m1()
