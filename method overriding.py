4db8d335ea18dd86b1bd708e73c7d6e178c465ef
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 13:02:04 +0530

    done

---
+++ method overriding.py
@@ -0,0 +1,4 @@
+#----------------------------METHOD OVERRIDING---------------------------
+
+# --> A parent containg some method that method by default available to the child class.
+# --> If child class want, then child class can provide own implementation for those method, this mechanism is called as method overriding.
