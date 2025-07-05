91b91a193f5386f7ce24321352055e61e9a100de
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 12:41:53 +0530

    done

--- constructor overloading .py
+++ constructor overloading .py
@@ -1,3 +1,18 @@
 #--------------CONSTRUCTOR OVERLOADING----------------------------
 # Constructor overloading is a mechanism in which a class can have any number of constructors with different no of parameters of different type of parameters. Generally It doesnot support but we overcome with multi dispatch module
 
+# example 1:
+# constructor overloading is not possible
+
+class student:
+    def __init__(self,a):
+        print(a)
+    def __init__(self,a,b):
+        print(a)
+        print(b)
+    def __init__(self,a,b,c):
+        print(a)
+        print(b)
+        print(c)
+s1=student(10)
+# here it will raise TypeError student.init missing two positional arguments..because we alreay know that python will takes the latest one....
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,11 @@
 class student:
-    def sample(self,*a):
+    def __init__(self,a):
         print(a)
-    def sample(self,*x):
-        print(x)
-s1=student()
-s1.sample(10)
-s1.sample(10,20,30)
-s1.sample(120,130,140,150,160)
+    def __init__(self,a,b):
+        print(a)
+        print(b)
+    def __init__(self,a,b,c):
+        print(a)
+        print(b)
+        print(c)
+s1=student(10)
