a3189a2f31c8a4eec6b4df914fb373a9f1390aab
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 13:15:31 +0530

    done

--- method overriding.py
+++ method overriding.py
@@ -23,3 +23,17 @@ class child(parent):
 c1=child()
 c1.sample()
 
+#Ex: 
+class Animal:
+    def sound(self):
+        print("Animals make different sounds")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog Barks")
+        
+class Cat(Animal):
+    def sound(self):
+        print("Cat Meows")
+        
+
