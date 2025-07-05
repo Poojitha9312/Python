a12445a4ee5f7848b0bd7902082812e746cab6cd
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 13:09:52 +0530

    done

--- method overriding.py
+++ method overriding.py
@@ -14,3 +14,30 @@ c1.sample()
 
 
 # example for method overriding
+
+class parent:
+    def sample(self):
+        print("I like fruits")
+    
+class child(parent):
+    def sample(self):
+        print("I like Ice cream and pizza")# Method overriding
+c1=child()
+c1.sample()
+
+# Another example of overriding:
+#Ex: 
+class Circle:
+    def area(self,r):
+        print(3.14*r*r)
+
+class Triangle(Circle):
+    def area(self,b,h):
+        print(0.5*b*h)
+        
+class Square(Circle):
+    def area(self,s):
+        print(s*s)
+        
+t=Triangle()
+t.area(2.3,5.6)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,9 @@
 class parent:
     def sample(self):
-        print(" I like fruits")
+        print("I like fruits")
+    
 class child(parent):
-    pass
+    def sample(self):
+        print("I like Ice cream and pizza")# Method overriding
 c1=child()
 c1.sample()
