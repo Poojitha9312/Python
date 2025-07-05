2e094bd3cec68573ae7afb96a8d9a9addd4b8cac
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 13:05:22 +0530

    done

--- method overriding.py
+++ method overriding.py
@@ -2,3 +2,14 @@
 
 # --> A parent containg some method that method by default available to the child class.
 # --> If child class want, then child class can provide own implementation for those method, this mechanism is called as method overriding.
+
+# Example 1:
+class parent:
+    def sample(self):
+        print(" I like fruits")
+class child(parent):
+    def sample1(self):
+        print("I like pizza")
+c1=child()
+c1.sample1()
+c1.sample()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,9 @@
-class student:
-    def __init__(self,a):
-        print(a)
-    def __init__(self,a,b):
-        print(a)
-        print(b)
-    def __init__(self,a,b,c):
-        print(a)
-        print(b)
-        print(c)
-s1=student(10)
+class parent:
+    def sample(self):
+        print(" I like fruits")
+class child(parent):
+    def sample1(self):
+        print("I like pizza")
+c1=child()
+c1.sample1()
+c1.sample()
