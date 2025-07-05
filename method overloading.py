7e1c44e1dd1f5a3c07b806a4bdeffbf6f3517a42
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 12:08:15 +0530

    done

--- method overloading.py
+++ method overloading.py
@@ -12,3 +12,27 @@
 ->Provide flexibility and easy to programmer.
 ->It reduce complexity.
 '''
+
+# Example 1:
+# 2 or more methods having same name but different number of arguments...
+class student:
+    def method1(self,a):
+        print(a)
+    def method1(self,a,b):
+        print(a)
+        print(b)
+    def method1(self,a,b,c):
+        print(a)
+        print(b)
+        print(c)
+
+s1=student()
+# s1.method1(10)#TypeError student.method1 missing 2 positional arguments
+# s1.method1(10,20)#TypeError student.method1 required one positional argument
+s1.method1(10,20,30)
+# here in this case python will takes the latest one 
+# python does not support method overloading directly...
+
+
+# Example 2:
+#How to archieve method overloading in python by using variable length arguments.
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,15 @@
-class employee:
-    def __init__(self,sal):
-        self.sal=sal
-    def __lt__(self,other):
-        return self.sal<other.exp
-class person:
-    def __init__(self,exp):
-        self.exp=exp
+class student:
+    def method1(self,a):
+        print(a)
+    def method1(self,a,b):
+        print(a)
+        print(b)
+    def method1(self,a,b,c):
+        print(a)
+        print(b)
+        print(c)
 
-e1=employee(40000)
-p1=person(45000)
-print(e1<p1)
+s1=student()
+# s1.method1(10)#TypeError student.method1 missing 2 positional arguments
+# s1.method1(10,20)#TypeError student.method1 required one positional argument
+s1.method1(10,20,30)
