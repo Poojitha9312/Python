0e7acb6a439afab775a374ace80959aa23ad6e33
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-05 12:43:13 +0530

    done

--- constructor overloading .py
+++ constructor overloading .py
@@ -16,3 +16,25 @@ class student:
         print(c)
 s1=student(10)
 # here it will raise TypeError student.init missing two positional arguments..because we alreay know that python will takes the latest one....
+
+# Example 2..
+# How to archieve Constructor overloading in python by using variable length arguments.
+class Test:
+    def __init__(self,*a):
+        print(a) 
+t=Test(10)
+t1=Test(10,20)
+
+# Constructor overloading using multidispatch module
+from multipledispatch import dispatch
+class Test:
+    @dispatch(int)
+    def __init__(self,a):
+        print(a)
+    
+    @dispatch(int,int)
+    def __init__(self,a,b):
+        print(a,b)
+        
+t=Test(10)
+t1=Test(10,20)
