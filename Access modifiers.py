bb1cc9d1fc7165220d2ea722d23d40fbfd369d59
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-07 22:27:56 +0530

    done

--- Access modifiers.py
+++ Access modifiers.py
@@ -1,2 +1,8 @@
 #--------------Access Modifiers---------------------
 # Access modifiers are used to control the scope of attributes and methods.........
+# There are three types of access modifiers in python...
+# They are:
+# 1.public
+# 2.private
+# 3.protected
+
