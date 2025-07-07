ac8c02c9f9a8ca5c9510b372497205cadcd701e3
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-07 22:46:04 +0530

    done

--- protected access modifiers.py
+++ protected access modifiers.py
@@ -1,1 +1,5 @@
 #-------------protected access modifiers----------------
+# protected modifiers can access
+# 1.within the class
+# 2.within the child class
+# 3.we can't access outside of the class...
