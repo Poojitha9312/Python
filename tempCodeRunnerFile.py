4349f57dcc4d6de1a6f0c2e102aaab5bd2d3f673
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-08 10:27:04 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,19 @@
-class College:
+class A:
     def __init__(self):
-        self.__balance=50000
-    def getbalance(self):
-        return self.__balance
-c=College()
-print(c.getbalance())
+        self.x=100 #public attribute
+    
+    def test(self):
+        print(self.x) #We are accessing public attribute with in the class.
+        
+class B(A):
+    def test1(self):
+        print(self.x) #We are accessing public attribute with in the child class
+        
+# a=A()
+# a.test()
+
+# b=B()
+# b.test1()
+
+a=A()
+print(a.x)
