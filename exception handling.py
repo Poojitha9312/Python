b7c0f3bce436f145177ecba47eaba4831a1d54ad
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-08 20:08:32 +0530

    done

---
+++ exception handling.py
@@ -0,0 +1,2 @@
+# In python if any exception will raise then PVM will create the corresponding exception object.
+# -->Then it will check corresponding handling code, if handling code available then no problem or else it will print the exception information and program will terminate abnormally.
