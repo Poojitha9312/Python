e3a32565610cd66c38fce851a6be5290cb6e73ce
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-10 13:34:32 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,4 @@
-class person:
-    def __init__(self,sal):
-        self.sal=sal
-    def __add__(self,x):
-        return self.sal+x.bonus
-class employee:
-    def __init__(self,bonus):
-        self.bonus=bonus
-
-p1=person(20000)
-e1=employee(5000)
-print(p1+e1)
+s1="  Rama  "
+print(len(s1))
+print(len(s1.strip()))
+print(s1.strip())
