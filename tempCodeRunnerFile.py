1e976fd73ccff40db91a345f2ef9a1e91f6e8da8
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-15 09:51:04 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-a=20
-def sample():
-    global a
-    a=a+15
-    print("The value of a is :", a)
-sample()
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
