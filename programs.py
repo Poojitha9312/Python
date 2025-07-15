27a11b867e2bb4c2f81548a67333de34f36485ec
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-15 20:43:59 +0530

    done

--- programs.py
+++ programs.py
@@ -17,5 +17,17 @@ if s==m:
 else:
     print("given number is not armstrong")
 
+#--------Factorial of given number--------
+'''The factorial of a number is the product of all the integers from 1 to that number.
+
+For example, the factorial of 6 is 1*2*3*4*5*6 = 720. Factorial is not defined for negative numbers, and the factorial of zero is one,0!=1 ''' 
+
+# by using for loop:
+n=int(input("enter n value:"))
+factorial=1
+for i in range(1,n+1):
+    factorial=factorial*i
+print(f"The factorial of {n} is {factorial}")
+
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,5 @@
-s=open('zsample.py',mode='w+')
-print(s.tell()) #tell is used to know the current cursor pointer place
-s.write("w+ mode")
-print(s.tell())
-s.seek(0) #seek is used to Move the file pointer to a specific position.
-print(s.tell())
-print(s.read())
-s.close()
+n=int(input("enter n value:"))
+factorial=1
+for i in range(1,n+1):
+    factorial=factorial*i
+print(f"The factorial of {n} is {factorial}")
