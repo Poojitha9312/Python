1262ec8b805e043ea6e3d59eb40c023fead42dc1
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-15 20:48:17 +0530

    done

--- programs.py
+++ programs.py
@@ -29,5 +29,14 @@ for i in range(1,n+1):
     factorial=factorial*i
 print(f"The factorial of {n} is {factorial}")
 
+# by using while loop:
+n=int(input("enter n value:"))
+factorial=1
+i=1
+while i<=n:
+    factorial=factorial*i
+    i=i+1
+print(f"The factorial of {n} is {factorial}")
+
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,7 @@
 n=int(input("enter n value:"))
 factorial=1
-for i in range(1,n+1):
+i=1
+while i<=n:
     factorial=factorial*i
+    i=i+1
 print(f"The factorial of {n} is {factorial}")
