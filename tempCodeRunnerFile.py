2fede8d08b6355869115c7c0468b636068dde024
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-16 10:08:21 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,5 @@
 n=5
 for i in range(n):
-    for j in range(i+1):
-        print(" ", end=" ")
-    for j in range(i,n):
+    for j in range(n):
         print("*",end=" ")
-    print() 
+    print()
