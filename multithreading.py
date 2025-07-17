0cf955041e408aaebe3488fb5a8a14bf5414374f
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-17 15:55:38 +0530

    done

---
+++ multithreading.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def sample(self,address):
+        print(f"My name is {self.name} and my age is {self.age} and my address is {address}")
+s1=student("ram",30)
+s1.sample("KKD")
