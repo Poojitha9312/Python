c15cecaccd9349528b1e372c4abe617a74c7fc5b
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-17 13:50:40 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,9 @@
 class student:
-    marks=100
-    def sample(self):
-        print("hello world")
-
-s1=student()
-print(s1.marks)
-s1.sample()
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        print(f"My name is {name} and my age is {age}")
+    def sample(self,address):
+        print(f" my address is {address}")
+s1=student("ram",30)
+s1.sample("KKD")
