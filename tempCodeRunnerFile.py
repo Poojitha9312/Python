0a5fa0b0e0ceb05818f125d5c57d692357600d24
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-17 13:45:18 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-l=[1,2,3,4,10,20,30]
-for i in range(3,6):
-   ele=eval(input(f" Enter the {i} th element of the list" ))
-   print(l.insert(i,ele))
-   print(l)
+class student:
+    marks=100
+    def sample(self):
+        print("hello world")
+
+s1=student()
+print(s1.marks)
+s1.sample()
