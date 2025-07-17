bf6c8cae71c99b25c885e30111e91fa692e11253
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-17 13:26:17 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
-l=[10,list({"name": "rama"}),list(((20,40)))]
-print(l)
-print(type(l))
+l=[1,2,3,4,10,20,30]
+for i in range(3,6):
+   ele=eval(input(f" Enter the {i} th element of the list" ))
+   print(l.insert(i,ele))
+   print(l)
--- zsample.py
+++ zsample.py
@@ -1,4 +1,1 @@
-l=[10,list({"name": "rama"}),list(((20,40)))]
-print(l)
-print(type(l))
 
