dc7dbadaf58d17802ddded3c269327d7b24f1088
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-18 09:13:54 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-class student:
-    marks=100
-    def sample(self,name,age):
-        print(f" My name is{name} and my age is {age}")
-s1=student()
-print(s1.marks)
-s1.sample()
+print("hello world")
