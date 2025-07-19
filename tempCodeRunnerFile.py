3455ce2e98695f8572db8b7794e4d9fa665ea136
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-19 14:12:30 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,11 @@
 class student:
-    class_var = "I am a class variable"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f"My name is {self.name} and my age is {self.age}"
+    def __repr__(self):
+        return f"My name is {self.name} and my age is {self.age}"
     
-    def method(self):
-        print("This is a method.")
-
-# Accessing the __dict__ attribute of the class
-print(student.__dict__)
+s1=student("snehi",15)
+print(s1)
