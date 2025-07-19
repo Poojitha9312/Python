103d9f870c22be1b81253ef440048acb10399712
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-19 14:19:44 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,9 +3,14 @@ class student:
         self.name=name
         self.age=age
     def __str__(self):
-        return f"My name is {self.name} and my age is {self.age}"
+        print("str method executes")
+        return f" My name is {self.name} and my age is {self.age}"
     def __repr__(self):
+        print("repr method executes")
         return f"My name is {self.name} and my age is {self.age}"
-    
 s1=student("snehi",15)
 print(s1)
+s2=student("ruchi",15)
+print(s2)
+s3=[(student("ram",30),student("krishna",29))]
+print(s3)
