9c00e9a28da3f74b6746c57bbaf4b026e8b5c5d2
Poojitha9312 <pujivasu2011@gmail.com>
2025-07-23 09:26:40 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,10 @@
-l=[i for i in range(11)]
-print(l)
+l=['h','e','r','a','r','s','p','k']
+def sample(y):
+    vowels=['a','e','i','o','u']
+    if (y in vowels):
+        return True
+    else:
+        False
+r=list(filter(sample,l))
+print(r)
+print(type(r))
