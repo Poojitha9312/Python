ef8ae37d6548bed99bf0702885f8473528272694
Poojitha9312 <pujivasu2011@gmail.com>
2025-08-30 11:57:24 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,2 @@
-class Test:
-    def m1(self):
-        for i in range(5):
-            print("Ram")
-    def m2(self):
-        for i in range(5):
-            print("Seetha")
-obj=Test()
-obj.m1()
-obj.m2()
+_name=10
+print(_name)
