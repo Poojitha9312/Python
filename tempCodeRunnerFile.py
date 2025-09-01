fdca497357d3bc93aa337caf8d97bc0542fd3927
Poojitha9312 <pujivasu2011@gmail.com>
2025-09-01 12:18:49 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,8 @@
-_name=10
-print(_name)
+f=True
+g=False
+print(type(g))
+print(type(f))
+print(bool(1))
+print(bool(0))    # except zero if we given any number the output bocomes true
+print(bool(8.2))
+print(bool(8+7j))
