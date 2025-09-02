d168e004ba94a4bdb51014e6002fd87de789be2d
Poojitha9312 <pujivasu2011@gmail.com>
2025-09-02 09:48:28 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,3 @@
-x=["apple","banana"]
-y=["apple","banana"]
-z=x
-
-# if the two variables are not pointing to same address location then is not operator returns true otherwise it returns false
-print(x is not z)
+x=10
+x/=3
+print(x)
