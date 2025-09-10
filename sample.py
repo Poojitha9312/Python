06eae7019bab3cb61d07404a3818ecf2f52bf436
Poojitha9312 <pujivasu2011@gmail.com>
2025-09-10 09:31:08 +0530

    done

--- sample.py
+++ sample.py
@@ -1,3 +1,20 @@
-for i in range(10):
-    pass
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
 
+
+# low-high:
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+#high-low:
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-for i in range(1,11):
-    for j in range(2,4) :
-        
-        print(i*j,end="  ")
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
     print()
