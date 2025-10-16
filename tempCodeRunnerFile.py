450f8c84cb2434ab18ce0aac9b55c34c47a33f36
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-16 15:57:24 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,11 @@
-d={1:"ram",2:"snehi",3:"ruchi"}
-print(d.items())
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def my_funct1(self):
+        print(f"My name is {self.name}")
+    def my_funct2(self):
+        print(f"My age is {self.age} ")
+s1=student("nikhi",20)
+s1.my_funct1()
+s1.my_funct2()
