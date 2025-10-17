64e0fc59c03e3b52e80cc29ed71ec9e0cb76e464
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-17 09:07:42 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,10 +2,7 @@ class student:
     def __init__(self,name,age):
         self.name=name
         self.age=age
-    def my_funct1(self):
-        print(f"My name is {self.name}")
-    def my_funct2(self):
-        print(f"My age is {self.age} ")
-s1=student("nikhi",20)
-s1.my_funct1()
-s1.my_funct2()
+    def sample(self,address):
+        print(f"My name is {self.name} and my age is {self.age} and my address is {address}")
+s1=student("ram",30)
+s1.sample("kkd")
