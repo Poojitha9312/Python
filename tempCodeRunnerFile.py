54ff3558b6433260612c9d55454ffddb7c614d55
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-18 11:23:58 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,17 @@
-class student():
-    def __init__(self):
-        print(id(self))
-        
-s1=student()
-print(id(s1))
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        print("here __str__method will executes") 
+        return f"My name is {self.name} and My age is {self.age}"
+    def __repr__(self):
+        print("here __repr__method will executes")
+        return f" My name is {self.name} and my age is {self.age}"
+s1=student("snehi",15)
+s2=student("vasu",30)
+print(s1)
+print(s2)
+
+s3=[(student("kavya",25),student("gopal",30))]
+print(s3)
