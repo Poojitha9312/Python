926ed57a88c64a104a16de34a2f4a8b1aebaa737
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-21 11:13:45 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,10 @@
-a=input("Enter the a value:")
-print(a)
-print(type(a))
-print(id(a))
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        def sample(self):
+            print(f"My name is {self.name}")
+        def sample2(self):
+            print(f"My age is {self.age}")
+s1=student("priya",20)
+print(student.__dict__)
