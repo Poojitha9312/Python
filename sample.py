3cfe4f44c185992c0ccdc444a586e7fc2474bde5
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-25 09:10:31 +0530

    done

--- sample.py
+++
@@ -1,6 +0,0 @@
-class student:
-    school_name="pargati"
-print(student.school_name)
-s1=student()
-del s1.school_name
-print(student.__dict__)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,13 @@
-class student:
-    school_name="hamsavahini"
-    def __init__(self):
-        del  student.school_name
-
-print(student.__dict__)
-s1=student()
-print(student.__dict__)
+class calculator:
+    def add(self):
+        a=10
+        b=20
+        result=a+b
+        print(result)
+        
+    def sub(self):
+        print(a)  #Here var a and b not works because they are local to add method
+        print(b)
+c1=calculator()
+c1.add()
+c1.sub()
