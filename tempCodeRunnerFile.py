2cd5cec4e51952ced01ae74de2c2d2c200bd1ba6
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-25 10:12:19 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,17 @@
-class calculator:
-    def add(self):
-        a=10
-        b=20
-        result=a+b
-        print(result)
-        
-    def sub(self):
-        print(a)  #Here var a and b not works because they are local to add method
-        print(b)
-c1=calculator()
-c1.add()
-c1.sub()
+class test:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def access(self):
+        print(f"The value of a is {self.a} and the value of b is {self.b}")
+    def update(self,x,y):
+        self.a=x
+        self.b=y
+    def delete(self):
+        del self.a
+t1=test(10,20)
+print(t1.__dict__)
+t1.access()
+t1.update(30,40)
+t1.delete()
+print(t1.__dict__)
