d2cd5177129dba7cab9c0eec898f4f715327819c
Poojitha9312 <pujivasu2011@gmail.com>
2025-10-27 10:38:50 +0530

    done

--- staticmethod.py
+++ staticmethod.py
@@ -3,7 +3,7 @@
 # self or cls variable not required as argument.
 # @staticmethod decarator we have to write before static method.
 # we can call one static method inside the static method.
-# Inside the class we can call trhe static method using the class name and outside of class we can call through the class name or object reference.
+# Inside the class we can call the static method using the class name and outside of class we can call through the class name or object reference.
 
 
 #Example 1:
