d78ef2d721a7399af9808971573e29fce84abd5a
Poojitha9312 <pujivasu2011@gmail.com>
2025-11-01 11:57:14 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,16 @@
-class A:
-    def m1(self):
-        self.x=10
-        print(f"Value or x is {self.x}")
-
-class B(A):
-    def m2(self):
-        self.x=20
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
         
+    def Details(self):
+        print(f'Name is {self.name}')
+        print(f'Age is {self.age}')
 
-b=B()
-b.m1()
+class Staff:
+    @classmethod
+    def modify_age(cls, x):  # class method (uses cls)
+        print(f'Modified Age: {x}')
+
+s1 = Student("Ram", 30)
+Staff.modify_age(s1.age)
