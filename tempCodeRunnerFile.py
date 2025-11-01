3a115dd0a0e331620c1fb20d0de7145c11b25213
Poojitha9312 <pujivasu2011@gmail.com>
2025-11-01 10:01:46 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,12 @@
-x=10
+class A:
+    def m1(self):
+        self.x=10
+        print(f"Value or x is {self.x}")
 
-x=9.2
-print(x)
+class B(A):
+    def m2(self):
+        self.x=20
+        
+
+b=B()
+b.m1()
