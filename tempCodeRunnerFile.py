0770c96dbf107109958765cb2ba045382b517653
Poojitha9312 <pujivasu2011@gmail.com>
2025-11-03 09:10:42 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,11 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        
-    def Details(self):
-        print(f'Name is {self.name}')
-        print(f'Age is {self.age}')
-
-class Staff:
-    @classmethod
-    def modify_age(cls, x):  # class method (uses cls)
-        print(f'Modified Age: {x}')
-
-s1 = Student("Ram", 30)
-Staff.modify_age(s1.age)
+class A:
+    def method1(self):
+        print('A class method')
+class B:
+    def method1(self):
+        print('B class Method')
+class C(B,A): #Here is the priority first A will execute.
+    pass
+    
+c=C()
+c.method1()
