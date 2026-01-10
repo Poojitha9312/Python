14c0ed7a42dcd1a0432c64893a4238dc397f627b
Poojitha9312 <pujivasu2011@gmail.com>
2026-01-10 09:06:32 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,2 @@
-def add(a,b):
-    yield a+b
-    yield a-b
-    yield a*b
-    yield a/b
-    yield a%b
-    yield a+20
-    yield b+40
-    yield a-2
-    yield a+1
-x=add(10,20)
-print(type(x))
-# here x is an generator object
-print(x.__next__())
-print(next(x))
-print(next(x))
-print(next(x))
+   print(x)
+s1=student("punar
