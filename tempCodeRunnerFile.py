ea346abab2822f515643f23c95a517c7fdcd6240
Poojitha9312 <pujivasu2011@gmail.com>
2026-02-27 08:56:34 +0530

    done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,10 @@
-s1="Rama"
-for i in enumerate(s1):
-    print(i)
+def even_odd(a):
+    """ This fun is used to check even or odd"""
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")  
+even_odd(10)
+even_odd(27)
+even_odd(40)
+print(even_odd.__doc__)
